bea9fbb54aacd02f47615087ca918ceedd24234e
BhavyaSri <ragalabhavyasri@gmail.com>
2026-05-25 23:36:35 +0530

    Fixed API routes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from ingestion.views import get_records, upload_sap_csv
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/', include('ingestion.urls')),
+    path('api/records/', get_records),
+    path('api/upload/', upload_sap_csv),
 ]
--- backend/ingestion/urls.py
+++ backend/ingestion/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import (
     upload_sap,
     get_records,
+    upload_utility,
+    upload_travel,
     approve_record
 )
 
@@ -10,4 +12,6 @@ urlpatterns = [
     path('upload/sap/', upload_sap),
     path('records/', get_records),
     path('records/<int:pk>/approve/', approve_record),
+    path('upload/utility/', upload_utility),
+    path('upload/travel/', upload_travel),
 ]
