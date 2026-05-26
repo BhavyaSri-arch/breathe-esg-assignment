5ba2d76cce29f6b5b5e2dbcc94fb77d84fdb06ed
BhavyaSri <ragalabhavyasri@gmail.com>
2026-05-26 11:15:28 +0530

    Fixed import error

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from ingestion.views import get_records, upload_sap_csv
+from ingestion.views import get_records
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('api/records/', get_records),
-    path('api/upload/', upload_sap_csv),
 ]
